1809680f9a7908ddb23b4212be8abb081e7782fe
rorychatt <pavel.rinne@gmail.com>
2025-09-28 16:53:46 +0200

    chore: create secrets

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ CultureInfo.DefaultThreadCurrentCulture = CultureInfo.DefaultThreadCurrentUICult
 var configuration = new ConfigurationBuilder()
     .SetBasePath(Directory.GetCurrentDirectory())
     .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-    .AddUserSecrets<Program>()
+    .AddUserSecrets("17bb6ff1-6c17-4fc0-b294-5427dfea29f3")
     .AddEnvironmentVariables()
     .Build();
 
